ae5d4745da8f69a2a709a32bd232b3fe13374de3
anjeelupreti <upretianjeel@gmail.com>
2025-12-13 01:58:42 +0545

    Add build.sh

--- core/settings.py
+++ core/settings.py
@@ -26,8 +26,11 @@ SECRET_KEY = os.getenv('SECRET_KEY','NOT-SET')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [
+    'zero1-himalayan-forest-resort.onrender.com',
+    'localhost',
+    '127.0.0.1'
+]
 
 # Application definition
 
